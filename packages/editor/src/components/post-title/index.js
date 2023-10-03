046cef6266f8ac4ac4aee777827546832456184b
Dave Smith <getdavemail@gmail.com>
2023-10-03 12:04:57 +0100

    Strip HTML from title on paste

--- packages/editor/src/components/post-title/index.js
+++ packages/editor/src/components/post-title/index.js
@@ -26,6 +26,7 @@ import {
 	insert,
 } from '@wordpress/rich-text';
 import { useMergeRefs } from '@wordpress/compose';
+import { __unstableStripHTML as stripHTML } from '@wordpress/dom';
 
 /**
  * Internal dependencies
@@ -169,7 +170,7 @@ function PostTitle( { rawText }, forwardedRef ) {
 				( firstBlock.name === 'core/heading' ||
 					firstBlock.name === 'core/paragraph' )
 			) {
-				onUpdate( firstBlock.attributes.content );
+				onUpdate( stripHTML( firstBlock.attributes.content ) );
 				onInsertBlockAfter( content.slice( 1 ) );
 			} else {
 				onInsertBlockAfter( content );
@@ -179,7 +180,10 @@ function PostTitle( { rawText }, forwardedRef ) {
 				...create( { html: title } ),
 				...selection,
 			};
-			const newValue = insert( value, create( { html: content } ) );
+			const newValue = insert(
+				value,
+				create( { html: stripHTML( content ) } )
+			);
 			onUpdate( toHTMLString( { value: newValue } ) );
 			setSelection( {
 				start: newValue.start,
