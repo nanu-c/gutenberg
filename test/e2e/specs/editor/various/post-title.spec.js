e6f22c40bcc6b5be43296f31dc1d814122850602
Dave Smith <getdavemail@gmail.com>
2023-10-05 09:53:57 +0100

    Add focus handling tests

--- test/e2e/specs/editor/various/post-title.spec.js
+++ test/e2e/specs/editor/various/post-title.spec.js
@@ -4,6 +4,48 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe( 'Post title', () => {
+	test.describe( 'Focus handling', () => {
+		test( 'should focus on the post title field when creating a new post in visual mode', async ( {
+			editor,
+			admin,
+		} ) => {
+			await admin.createNewPost();
+
+			const pageTitleField = editor.canvas.getByRole( 'textbox', {
+				name: 'Add title',
+			} );
+
+			await expect( pageTitleField ).toBeFocused();
+		} );
+
+		// Known bug. Will need TextareaControl to expose a ref for this to
+		// work.
+		// eslint-disable-next-line playwright/no-skipped-test
+		test.skip( 'should focus on the post title field when creating a new post in code editor mode', async ( {
+			editor,
+			admin,
+			pageUtils,
+		} ) => {
+			await admin.createNewPost();
+
+			// switch Editor to code editor mode
+			// Open code editor
+			await pageUtils.pressKeys( 'secondary+M' ); // Emulates CTRL+Shift+Alt + M => toggle code editor
+
+			// Check we're in Code view mode.
+			await expect(
+				editor.canvas.getByRole( 'heading', {
+					name: 'Editing code',
+				} )
+			).toBeVisible();
+
+			const pageTitleField = editor.canvas.getByRole( 'textbox', {
+				name: 'Add title',
+			} );
+
+			await expect( pageTitleField ).toBeFocused();
+		} );
+	} );
 	test.describe( 'HTML handling', () => {
 		test( `should (visually) render any HTML in Post Editor's post title field when in Visual editing mode`, async ( {
 			page,
@@ -83,7 +125,7 @@ test.describe( 'Post title', () => {
 					.set( 'core/edit-post', 'fullscreenMode', false );
 			}, false );
 
-			// switch Editor to code view mode
+			// switch Editor to code editor mode
 			// Open code editor
 			await pageUtils.pressKeys( 'secondary+M' ); // Emulates CTRL+Shift+Alt + M => toggle code editor
 
@@ -144,7 +186,7 @@ test.describe( 'Post title', () => {
 		} ) => {
 			await admin.createNewPost();
 
-			// switch Editor to code view mode
+			// switch Editor to code editor mode
 			// Open code editor
 			await pageUtils.pressKeys( 'secondary+M' ); // Emulates CTRL+Shift+Alt + M => toggle code editor
 
