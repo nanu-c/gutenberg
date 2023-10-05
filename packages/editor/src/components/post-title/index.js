e0855f2ffd73fb3d8fd76f00e5d55fc58ff349b9
Dave Smith <getdavemail@gmail.com>
2023-10-05 09:54:30 +0100

    Extract Rich Post Title component and refactor

--- packages/editor/src/components/post-title/index.js
+++ packages/editor/src/components/post-title/index.js
@@ -40,31 +40,173 @@ import { store as editorStore } from '../../store';
  */
 const REGEXP_NEWLINES = /[\r\n]+/g;
 
-const PostTitleRaw = forwardRef(
-	( { title, placeholder, onChange, className }, ref ) => {
+const PostTitleRaw = ( { title, placeholder, onChange, className } ) => {
+	return (
+		<TextareaControl
+			value={ title }
+			onChange={ onChange }
+			label={ placeholder }
+			className={ className }
+			placeholder={ placeholder }
+			hideLabelFromVision={ true }
+			autoComplete="off"
+			dir="auto"
+			__nextHasNoMarginBottom
+		/>
+	);
+};
+
+const PostTitleRich = forwardRef(
+	(
+		{ title, placeholder, onChange, onUpdate, className, setIsSelected },
+		ref
+	) => {
+		const [ selection, setSelection ] = useState( {} );
+
+		const { clearSelectedBlock, insertBlocks, insertDefaultBlock } =
+			useDispatch( blockEditorStore );
+
+		function onInsertBlockAfter( blocks ) {
+			insertBlocks( blocks, 0 );
+		}
+
+		function onSelect() {
+			setIsSelected( true );
+			clearSelectedBlock();
+		}
+
+		function onUnselect() {
+			setIsSelected( false );
+			setSelection( {} );
+		}
+
+		function onEnterPress() {
+			insertDefaultBlock( undefined, undefined, 0 );
+		}
+
+		function onKeyDown( event ) {
+			if ( event.keyCode === ENTER ) {
+				event.preventDefault();
+				onEnterPress();
+			}
+		}
+
+		function onPaste( event ) {
+			const clipboardData = event.clipboardData;
+
+			let plainText = '';
+			let html = '';
+
+			// IE11 only supports `Text` as an argument for `getData` and will
+			// otherwise throw an invalid argument error, so we try the standard
+			// arguments first, then fallback to `Text` if they fail.
+			try {
+				plainText = clipboardData.getData( 'text/plain' );
+				html = clipboardData.getData( 'text/html' );
+			} catch ( error1 ) {
+				try {
+					html = clipboardData.getData( 'Text' );
+				} catch ( error2 ) {
+					// Some browsers like UC Browser paste plain text by default and
+					// don't support clipboardData at all, so allow default
+					// behaviour.
+					return;
+				}
+			}
+
+			// Allows us to ask for this information when we get a report.
+			window.console.log( 'Received HTML:\n\n', html );
+			window.console.log( 'Received plain text:\n\n', plainText );
+
+			const content = pasteHandler( {
+				HTML: html,
+				plainText,
+			} );
+
+			event.preventDefault();
+
+			if ( ! content.length ) {
+				return;
+			}
+
+			if ( typeof content !== 'string' ) {
+				const [ firstBlock ] = content;
+
+				if (
+					! title &&
+					( firstBlock.name === 'core/heading' ||
+						firstBlock.name === 'core/paragraph' )
+				) {
+					onUpdate( stripHTML( firstBlock.attributes.content ) );
+					onInsertBlockAfter( content.slice( 1 ) );
+				} else {
+					onInsertBlockAfter( content );
+				}
+			} else {
+				const value = {
+					...create( { html: title } ),
+					...selection,
+				};
+				const newValue = insert(
+					value,
+					create( { html: stripHTML( content ) } )
+				);
+				onUpdate( toHTMLString( { value: newValue } ) );
+				setSelection( {
+					start: newValue.start,
+					end: newValue.end,
+				} );
+			}
+		}
+
+		const { ref: richTextRef } = useRichText( {
+			value: title,
+			onChange,
+			placeholder,
+			selectionStart: selection.start,
+			selectionEnd: selection.end,
+			onSelectionChange( newStart, newEnd ) {
+				setSelection( ( sel ) => {
+					const { start, end } = sel;
+					if ( start === newStart && end === newEnd ) {
+						return sel;
+					}
+					return {
+						start: newStart,
+						end: newEnd,
+					};
+				} );
+			},
+			__unstableDisableFormats: false,
+			preserveWhiteSpace: true,
+		} );
+
 		return (
-			<TextareaControl
-				ref={ ref }
-				value={ title }
-				onChange={ onChange }
-				label={ placeholder }
-				hideLabelFromVision={ true }
-				className={ className }
-				autoComplete="off"
-				dir="auto"
-				placeholder={ placeholder }
-				__nextHasNoMarginBottom
-			/>
+			/* eslint-disable jsx-a11y/heading-has-content, jsx-a11y/no-noninteractive-element-to-interactive-role */
+			<PostTypeSupportCheck supportKeys="title">
+				<h1
+					ref={ useMergeRefs( [ richTextRef, ref ] ) }
+					contentEditable
+					className={ className }
+					aria-label={ placeholder }
+					role="textbox"
+					aria-multiline="true"
+					onFocus={ onSelect }
+					onBlur={ onUnselect }
+					onKeyDown={ onKeyDown }
+					onKeyPress={ onUnselect }
+					onPaste={ onPaste }
+				/>
+			</PostTypeSupportCheck>
+			/* eslint-enable jsx-a11y/heading-has-content, jsx-a11y/no-noninteractive-element-to-interactive-role */
 		);
 	}
 );
 
 function PostTitle( { rawText }, forwardedRef ) {
 	const ref = useRef();
-	const [ isSelected, setIsSelected ] = useState( false );
+
 	const { editPost } = useDispatch( editorStore );
-	const { insertDefaultBlock, clearSelectedBlock, insertBlocks } =
-		useDispatch( blockEditorStore );
 	const { isCleanNewPost, title, placeholder, hasFixedToolbar } = useSelect(
 		( select ) => {
 			const { getEditedPostAttribute, isCleanNewPost: _isCleanNewPost } =
@@ -82,6 +224,8 @@ function PostTitle( { rawText }, forwardedRef ) {
 		},
 		[]
 	);
+
+	const [ isSelected, setIsSelected ] = useState( false );
 
 	useImperativeHandle( forwardedRef, () => ( {
 		focus: () => {
@@ -109,107 +253,12 @@ function PostTitle( { rawText }, forwardedRef ) {
 		}
 	}, [ isCleanNewPost ] );
 
-	function onEnterPress() {
-		insertDefaultBlock( undefined, undefined, 0 );
-	}
-
-	function onInsertBlockAfter( blocks ) {
-		insertBlocks( blocks, 0 );
-	}
-
 	function onUpdate( newTitle ) {
 		editPost( { title: newTitle } );
 	}
 
-	const [ selection, setSelection ] = useState( {} );
-
-	function onSelect() {
-		setIsSelected( true );
-		clearSelectedBlock();
-	}
-
-	function onUnselect() {
-		setIsSelected( false );
-		setSelection( {} );
-	}
-
 	function onChange( value ) {
 		onUpdate( value.replace( REGEXP_NEWLINES, ' ' ) );
-	}
-
-	function onKeyDown( event ) {
-		if ( event.keyCode === ENTER ) {
-			event.preventDefault();
-			onEnterPress();
-		}
-	}
-
-	function onPaste( event ) {
-		const clipboardData = event.clipboardData;
-
-		let plainText = '';
-		let html = '';
-
-		// IE11 only supports `Text` as an argument for `getData` and will
-		// otherwise throw an invalid argument error, so we try the standard
-		// arguments first, then fallback to `Text` if they fail.
-		try {
-			plainText = clipboardData.getData( 'text/plain' );
-			html = clipboardData.getData( 'text/html' );
-		} catch ( error1 ) {
-			try {
-				html = clipboardData.getData( 'Text' );
-			} catch ( error2 ) {
-				// Some browsers like UC Browser paste plain text by default and
-				// don't support clipboardData at all, so allow default
-				// behaviour.
-				return;
-			}
-		}
-
-		// Allows us to ask for this information when we get a report.
-		window.console.log( 'Received HTML:\n\n', html );
-		window.console.log( 'Received plain text:\n\n', plainText );
-
-		const content = pasteHandler( {
-			HTML: html,
-			plainText,
-		} );
-
-		event.preventDefault();
-
-		if ( ! content.length ) {
-			return;
-		}
-
-		if ( typeof content !== 'string' ) {
-			const [ firstBlock ] = content;
-
-			if (
-				! title &&
-				( firstBlock.name === 'core/heading' ||
-					firstBlock.name === 'core/paragraph' )
-			) {
-				onUpdate( stripHTML( firstBlock.attributes.content ) );
-				onInsertBlockAfter( content.slice( 1 ) );
-			} else {
-				onInsertBlockAfter( content );
-			}
-		} else {
-			const value = {
-				...create( { html: title } ),
-				...selection,
-			};
-			const newValue = insert(
-				value,
-				create( { html: stripHTML( content ) } )
-			);
-			onUpdate( toHTMLString( { value: newValue } ) );
-			setSelection( {
-				start: newValue.start,
-				end: newValue.end,
-			} );
-		}
 	}
 
 	// The wp-block className is important for editor styles.
@@ -225,34 +274,9 @@ function PostTitle( { rawText }, forwardedRef ) {
 	const decodedPlaceholder =
 		decodeEntities( placeholder ) || __( 'Add title' );
 
-	const { ref: richTextRef } = useRichText( {
-		value: title,
-		onChange,
-		placeholder: decodedPlaceholder,
-		selectionStart: selection.start,
-		selectionEnd: selection.end,
-		onSelectionChange( newStart, newEnd ) {
-			setSelection( ( sel ) => {
-				const { start, end } = sel;
-				if ( start === newStart && end === newEnd ) {
-					return sel;
-				}
-				return {
-					start: newStart,
-					end: newEnd,
-				};
-			} );
-		},
-		__unstableDisableFormats: false,
-		preserveWhiteSpace: true,
-	} );
-
-	const richEditorRef = useMergeRefs( [ richTextRef, ref ] );
-
 	if ( rawText ) {
 		return (
 			<PostTitleRaw
-				ref={ ref }
 				title={ title }
 				className={ className }
 				placeholder={ decodedPlaceholder }
@@ -260,25 +284,17 @@ function PostTitle( { rawText }, forwardedRef ) {
 		);
 	}
 
-	/* eslint-disable jsx-a11y/heading-has-content, jsx-a11y/no-noninteractive-element-to-interactive-role */
 	return (
-		<PostTypeSupportCheck supportKeys="title">
-			<h1
-				ref={ richEditorRef }
-				contentEditable
-				className={ className }
-				aria-label={ decodedPlaceholder }
-				role="textbox"
-				aria-multiline="true"
-				onFocus={ onSelect }
-				onBlur={ onUnselect }
-				onKeyDown={ onKeyDown }
-				onKeyPress={ onUnselect }
-				onPaste={ onPaste }
-			/>
-		</PostTypeSupportCheck>
+		<PostTitleRich
+			ref={ ref }
+			title={ title }
+			onChange={ onChange }
+			onUpdate={ onUpdate }
+			className={ className }
+			placeholder={ decodedPlaceholder }
+			setIsSelected={ setIsSelected }
+		/>
 	);
-	/* eslint-enable jsx-a11y/heading-has-content, jsx-a11y/no-noninteractive-element-to-interactive-role */
 }
 
 export default forwardRef( PostTitle );
