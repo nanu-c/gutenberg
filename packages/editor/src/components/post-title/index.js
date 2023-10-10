0907f9fc4c19178b86048f6976548577a2920adc
Dave Smith <getdavemail@gmail.com>
2023-10-10 10:04:07 +0100

    Extract focus management to hook

--- packages/editor/src/components/post-title/index.js
+++ packages/editor/src/components/post-title/index.js
@@ -7,13 +7,7 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import {
-	forwardRef,
-	useEffect,
-	useImperativeHandle,
-	useRef,
-	useState,
-} from '@wordpress/element';
+import { forwardRef, useState } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
 import { useSelect, useDispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
@@ -24,56 +18,26 @@ import { store as blockEditorStore } from '@wordpress/block-editor';
 import { store as editorStore } from '../../store';
 import PostTitleRich from './post-title-rich';
 import { DEFAULT_CLASSNAMES, REGEXP_NEWLINES } from './constants';
+import usePostTitleFocus from './use-post-title-focus';
 
 function PostTitle( _, forwardedRef ) {
-	const ref = useRef();
-
 	const { editPost } = useDispatch( editorStore );
-	const { isCleanNewPost, title, placeholder, hasFixedToolbar } = useSelect(
-		( select ) => {
-			const { getEditedPostAttribute, isCleanNewPost: _isCleanNewPost } =
-				select( editorStore );
-			const { getSettings } = select( blockEditorStore );
-			const { titlePlaceholder, hasFixedToolbar: _hasFixedToolbar } =
-				getSettings();
+	const { title, placeholder, hasFixedToolbar } = useSelect( ( select ) => {
+		const { getEditedPostAttribute } = select( editorStore );
+		const { getSettings } = select( blockEditorStore );
+		const { titlePlaceholder, hasFixedToolbar: _hasFixedToolbar } =
+			getSettings();
 
-			return {
-				isCleanNewPost: _isCleanNewPost(),
-				title: getEditedPostAttribute( 'title' ),
-				placeholder: titlePlaceholder,
-				hasFixedToolbar: _hasFixedToolbar,
-			};
-		},
-		[]
-	);
+		return {
+			title: getEditedPostAttribute( 'title' ),
+			placeholder: titlePlaceholder,
+			hasFixedToolbar: _hasFixedToolbar,
+		};
+	}, [] );
 
 	const [ isSelected, setIsSelected ] = useState( false );
 
-	useImperativeHandle( forwardedRef, () => ( {
-		focus: () => {
-			ref?.current?.focus();
-		},
-	} ) );
-
-	useEffect( () => {
-		if ( ! ref.current ) {
-			return;
-		}
-
-		const { defaultView } = ref.current.ownerDocument;
-		const { name, parent } = defaultView;
-		const ownerDocument =
-			name === 'editor-canvas' ? parent.document : defaultView.document;
-		const { activeElement, body } = ownerDocument;
-
-		// Only autofocus the title when the post is entirely empty. This should
-		// only happen for a new post, which means we focus the title on new
-		// post so the author can start typing right away, without needing to
-		// click anything.
-		if ( isCleanNewPost && ( ! activeElement || body === activeElement ) ) {
-			ref.current.focus();
-		}
-	}, [ isCleanNewPost ] );
+	const { ref: focusRef } = usePostTitleFocus( forwardedRef );
 
 	function onUpdate( newTitle ) {
 		editPost( { title: newTitle } );
@@ -95,7 +59,7 @@ function PostTitle( _, forwardedRef ) {
 
 	return (
 		<PostTitleRich
-			ref={ ref }
+			ref={ focusRef }
 			title={ title }
 			onChange={ onChange }
 			onUpdate={ onUpdate }
