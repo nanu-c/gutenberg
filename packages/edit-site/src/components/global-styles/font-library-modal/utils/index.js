ee06d0e28a10a3c4c8d3a848cbd12d542b17e623
Jeff Ong <jonger4@gmail.com>
2023-10-16 14:05:21 -0400

    Wrap fontFamily in quotes.

--- packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/index.js
@@ -149,7 +149,24 @@ export function makeFormDataFromFontFamilies( fontFamilies ) {
 				}
 				return face;
 			} );
+			const familyNames = family.fontFamily.split( ',' ).map( familyName => familyName.trim() );
+	        const formattedFontFamilies = familyNames.map( name => {
+				if (
+					name.includes(' ') &&
+					!name.includes('"') &&
+					!name.includes("'")
+				) {
+					return `'${name}'`;
+				} else {
+					return name;
+				}
+			});
+
+			if ( formattedFontFamilies.length > 1 ) {
+				family.fontFamily = formattedFontFamilies.join(', ');
+			}
 		}
+
 		return family;
 	} );
 	formData.append( 'font_families', JSON.stringify( newFontFamilies ) );
